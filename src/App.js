7f118000a2e1366f31222028ce2f6f2f042fab89
wilsonmike <michaelwilsonsj@hotmail.com>
2021-08-06 16:30:28 -0400

    added react router and users page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import Users from "./user/pages/Users";
 
 function App() {
-  return <h1>Hi</h1>;
+  return (
+    <Router>
+      <Route path="/" exact>
+        <Users />
+      </Route>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/user/pages/Users.js
@@ -0,0 +1,5 @@
+function Users() {
+  return <h2>Users Works</h2>;
+}
+
+export default Users;
