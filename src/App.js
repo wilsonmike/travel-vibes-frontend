2edb706004db7002d0aec7e52ba9d8734ee172a6
wilsonmike <michaelwilsonsj@hotmail.com>
2021-08-08 17:44:52 -0400

    dummy data displayed on UserPlaces

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import {
 
 import Users from "./user/pages/Users";
 import NewPlace from "./places/pages/NewPlace";
+import UserPlaces from "./places/pages/UserPlaces";
 import MainNavigation from "./shared/components/Navigation/MainNavigation";
 
 function App() {
@@ -15,15 +16,18 @@ function App() {
     <Router>
       <MainNavigation />
       <main>
-      <Switch>
-        <Route path="/" exact>
-          <Users />
-        </Route>
-        <Route path="/places/new" exact>
-          <NewPlace />
-        </Route>
-        <Redirect to="/" />
-      </Switch>
+        <Switch>
+          <Route path="/" exact>
+            <Users />
+          </Route>
+          <Route path="/:userId/places" exact>
+            <UserPlaces />
+          </Route>
+          <Route path="/places/new" exact>
+            <NewPlace />
+          </Route>
+          <Redirect to="/" />
+        </Switch>
       </main>
     </Router>
   );
--- src/places/pages/UserPlaces.js
+++ src/places/pages/UserPlaces.js
@@ -1,5 +1,36 @@
+import PlaceList from "../components/PlaceList";
+
+const DUMMY_PLACES = [
+  {
+    id: "p1",
+    title: "Empire State Building",
+    description: "One of the most famous skyscrapers",
+    imageUrl:
+      "https://images.pexels.com/photos/755050/pexels-photo-755050.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    address: "20 W 34th St, New York, NY 10001",
+    location: {
+      lat: 40.7484405,
+      lng: -73.9878584,
+    },
+    creator: "u1",
+  },
+  {
+    id: "p2",
+    title: "Empire State Building",
+    description: "One of the most famous skyscrapers",
+    imageUrl:
+      "https://images.pexels.com/photos/755050/pexels-photo-755050.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    address: "20 W 34th St, New York, NY 10001",
+    location: {
+      lat: 40.7484405,
+      lng: -73.9878584,
+    },
+    creator: "u2",
+  },
+];
+
 function UserPlaces() {
-  return <h1>user places works</h1>;
+  return <PlaceList items={DUMMY_PLACES} />;
 }
 
 export default UserPlaces;
