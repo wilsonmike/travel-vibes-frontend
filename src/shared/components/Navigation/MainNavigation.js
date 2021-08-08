3a11f615233f09470ad4ca7f9ce823b8bacb288f
wilsonmike <michaelwilsonsj@hotmail.com>
2021-08-08 16:54:46 -0400

    open and close mobile nav && clsoe the nav on link press (open close handler)

--- src/shared/components/Navigation/MainNavigation.js
+++ src/shared/components/Navigation/MainNavigation.js
@@ -1,47 +1,49 @@
-import MainHeader from './MainHeader';
-import { Link } from 'react-router-dom';
-import { useState } from 'react';
-import NavLinks from './NavLinks';
-import SideDrawer from './SideDrawer';
-import Backdrop from '../../components/UIElements/Backdrop';
-import './MainNavigation.css'
-import React from 'react';
+import MainHeader from "./MainHeader";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import NavLinks from "./NavLinks";
+import SideDrawer from "./SideDrawer";
+import Backdrop from "../../components/UIElements/Backdrop";
+import "./MainNavigation.css";
+import React from "react";
 
 function MainNavigation(props) {
-    const [drawerIsOpen, setDrawerIsOpen] = useState(false);
+  const [drawerIsOpen, setDrawerIsOpen] = useState(false);
 
-    function openDrawer(){
-        setDrawerIsOpen(true)
-    }
+  function openDrawerHandler() {
+    setDrawerIsOpen(true);
+  }
 
-    function closeDrawer() {
-        setDrawerIsOpen(false)
-    }
+  function closeDrawerHandler() {
+    setDrawerIsOpen(false);
+  }
 
-
-    return (
-        <React.Fragment>
-            {drawerIsOpen && <Backdrop onClick={closeDrawer} />}
-        {drawerIsOpen ? (<SideDrawer>
-            <nav className="main-navigation__drawer-nav">
-                <NavLinks />
-            </nav>
-        </SideDrawer>) : null}
-    <MainHeader>
-        <button className="main-navigation__menu-btn" onClick={openDrawer}>
-            <span />
-            <span />
-            <span />
+  return (
+    <React.Fragment>
+      {drawerIsOpen && <Backdrop onClick={closeDrawerHandler} />}
+      <SideDrawer show={drawerIsOpen} onClick={closeDrawerHandler}>
+        <nav className="main-navigation__drawer-nav">
+          <NavLinks />
+        </nav>
+      </SideDrawer>
+      <MainHeader>
+        <button
+          className="main-navigation__menu-btn"
+          onClick={openDrawerHandler}
+        >
+          <span />
+          <span />
+          <span />
         </button>
         <h1 className="main-navigation__title">
-            <Link to="/">Place Vibe</Link>
+          <Link to="/">Place Vibe</Link>
         </h1>
         <nav className="main-navigation__header-nav">
-           <NavLinks /> 
+          <NavLinks />
         </nav>
-    </MainHeader>
+      </MainHeader>
     </React.Fragment>
-    )
+  );
 }
 
 export default MainNavigation;
