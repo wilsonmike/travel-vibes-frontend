928bf8a8588c61ba79e6c83f7ab8672a94315a6b
wilsonmike <michaelwilsonsj@hotmail.com>
2021-08-07 20:36:28 -0400

    added avatar shared uielement && styling

--- src/user/components/UserItem.js
+++ src/user/components/UserItem.js
@@ -1,13 +1,18 @@
+import "./UserItem.css";
+import Avatar from "../../shared/components/UIElements/Avatar";
+
 function UserItem(props) {
   return (
     <li className="user-item">
       <div className="user-item__content">
         <div className="user-item__image">
-          <img src={props.image} alt={props.name} />
+          <Avatar image={props.image} alt={props.name} />
         </div>
         <div className="user-item__info">
           <h2>{props.name}</h2>
-          <h3>{props.placeCount}</h3>
+          <h3>
+            {props.placeCount} {props.placeCount === 1 ? "Place" : "Places"}
+          </h3>
         </div>
       </div>
     </li>
