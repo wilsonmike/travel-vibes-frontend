062469da5fafbf1b73691045ef31376a32baacd3
wilsonmike <michaelwilsonsj@hotmail.com>
2021-08-07 23:42:35 -0400

    starting nav - added main header in shared

---
+++ src/shared/components/Navigation/MainHeader.js
@@ -0,0 +1,7 @@
+import "./MainHeader.css";
+
+function MainHeader() {
+  return <h1>Main Header Works</h1>;
+}
+
+export default MainHeader;
--- src/user/components/UsersList.js
+++ src/user/components/UsersList.js
@@ -1,11 +1,14 @@
 import "./UsersList.css";
 import UserItem from "./UserItem";
+import Card from "../../shared/components/UIElements/Card";
 
 function UsersList(props) {
   if (props.items.length === 0) {
     return (
       <div className="center">
-        <h2>No users found.</h2>
+        <Card>
+          <h2>No users found.</h2>
+        </Card>
       </div>
     );
   }
